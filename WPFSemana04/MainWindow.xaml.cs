26a51b02381da10b00e601766cbc104a7e5237f3
renato10 <juan.flores.p@tecsup.edu.pe>
2022-09-13 22:29:38 -0500

    first commit

---
+++ WPFSemana04/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFSemana04
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection connection = new SqlConnection(@"data source=DESKTOP-5GRTRAP\SQLEXPRESS2017;initial catalog = School; Integrated Security=True;");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvPeople_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Person> people = new List<Person>();
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("USP_GetPeople", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter parameter1 = new SqlParameter();
+            parameter1.SqlDbType = SqlDbType.VarChar;
+            parameter1.Size = 50;
+
+            parameter1.Value = "";
+            parameter1.ParameterName = "@LastName";
+
+            SqlParameter parameter2 = new SqlParameter();
+            parameter2.SqlDbType = SqlDbType.VarChar;
+            parameter2.Size = 50;
+
+            parameter2.Value = "";
+            parameter2.ParameterName = "@FirstName";
+
+            command.Parameters.Add(parameter1);
+            command.Parameters.Add(parameter2);
+
+            SqlDataReader dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+
+                people.Add(new Person
+                {
+                    PersonId = dataReader["PeopleID"].ToString(),
+                    LastName = dataReader["LastName"].ToString(),
+                    FirstName = dataReader["FirstName"].ToString(),
+                    FullName = String.Concat(dataReader["FirstName"].ToString(), "",
+                    dataReader["LastName"].ToString())
+                });
+            }
+
+            connection.Close();
+            dgvPeople.ItemsSource = people;
+        }
+    }
+
+    public class Person
+    {
+        public string PersonId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+    }
+}
